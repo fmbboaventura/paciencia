8faaef738b6dc4899aa455dca0e88223560f10e1
gadielxavier <gadiel.xavier@gmail.com>
2018-07-04 22:59:15 -0300

    Adicionados VerificadorPadraoFileira e VerificadorPadraoFundacao

---
+++ src/paciencia/model/baralho/VerificadorPadraoFileira.java
@@ -0,0 +1,16 @@
+package paciencia.model.baralho;
+
+public class VerificadorPadraoFileira implements Verificador{
+
+	@Override
+	public boolean verificar(Carta carta1, Carta carta2) {
+		/**
+		 * Recebe cartas na ordem crescente e do mesmo naipe.
+		 * if(carta1.compararCor(carta2) && carta1.getValor() - carta2.getValor() == 1)
+			return true
+		 */
+		
+		return false;
+	}
+
+}
---
+++ src/paciencia/model/baralho/VerificadorPadraoFundacao.java
@@ -0,0 +1,15 @@
+package paciencia.model.baralho;
+
+public class VerificadorPadraoFundacao {
+	
+	public boolean verificar(Carta carta1, Carta carta2) {
+		/**
+		 * Aceita cartas na ordem decrescente e da cor oposta
+		 * if(!carta1.compararCor(carta2) && carta2.getValor() - carta1.getValor() == 1)
+			return true
+		 */
+		
+		return false;
+	}
+
+}
